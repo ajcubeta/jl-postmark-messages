080450cbc505f69eae9e795787cf3c4889871aba
Andre Joseph <ajcubeta@gmail.com>
2017-10-11 18:01:18 +0800

    Check webhook payload on outbound webhook

--- app/controllers/outbound_webhooks_controller.rb
+++ app/controllers/outbound_webhooks_controller.rb
@@ -17,7 +17,7 @@ class OutboundWebhooksController < ApplicationController
     @webhook = OutboundWebhook.new(payload: request.body.read, webhook_type: 'Delivered')
 
     if @webhook.save
-      Rails.logger.info "MessageID is #{@webhook.message_id}"
+      Rails.logger.info "Webhook ID: #{@webhook.id} and MessageID is #{@webhook.payload["MessageID"]}"
       @webhook.migrate_message!
       render json: @webhook, status: :created
     else
